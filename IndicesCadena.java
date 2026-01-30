e1146d7e3574564197bcddfc9415f26f793feb26
Zequiel92 <fantamigosmg@gmail.com>
2026-01-30 11:16:00 -0300

    Manejo de indices

---
+++ IndicesCadena.java
@@ -0,0 +1,21 @@
+public class IndicesCadena {
+
+  public static void main(String[] args) {
+    
+    // Manejo de indices de cadenas
+    var cadena1 = "Hola Mundo";
+
+
+    // Recuperar el primer caracter
+    var primerCaracter = cadena1.charAt(0);
+    System.out.println(primerCaracter);
+
+    // Recuperar el ultimo caracter
+    var ultimoCaracter = cadena1.charAt(9);
+    System.out.println(ultimoCaracter);
+
+    // Recuperar la letra M 
+    var caracterM = cadena1.charAt(5);
+    System.out.println(caracterM);
+  }
+}
