e279b84ba3d251c6c6071d9a807cb34546652dac
Zequiel92 <fantamigosmg@gmail.com>
2026-04-16 18:44:42 -0300

    Ejercitando Ciclos while y Do While

---
+++ CiclosSwitch.java
@@ -0,0 +1,26 @@
+public class CiclosSwitch {
+  public static void main(String[] args) {
+    
+    int dia = 3;
+
+    switch (dia) {
+      case 1:
+        System.out.println("Lunes");
+        break;
+      case 2:
+        System.out.println("Martes");
+        break;
+
+      case 3:
+        System.out.println("Miercoles");
+        break;
+
+      case 4:
+        System.out.println("Jueves");
+        break;
+
+      default:
+        break;
+    }
+  }
+}
