973be9cc77a3c4e9bf4e6155cf4f831f45b433d7
Zequiel92 <fantamigosmg@gmail.com>
2026-04-28 02:44:05 -0300

    practica con for

---
+++ CiclosFor.java
@@ -0,0 +1,11 @@
+public class CiclosFor {
+  public static void main(String[] args) {
+    
+    // Ciclos For
+    int [] mi_arreglo = {1,2,3,4};
+
+    for (int i = 0;i >= 4;i++) {
+      System.out.println(i);
+    }
+  }
+}
